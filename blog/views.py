1c1f9b4941936c4bd583589bf176382446abd451
Adam Joyce <adamjoyce@outlook.com>
2017-11-08 21:41:27 +0000

    Category navigation tabs and panels implemented

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 
-from .models import Post
+from .models import Category, Post
 
-def post_list(request):
+def index(request):
     context_dict = {}
+    categories = Category.objects.all()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
             '-published_date')
     featured_post = posts[0]
+    context_dict['categories'] = categories
     context_dict['posts'] = posts
     context_dict['featured_post'] = featured_post
-    return render(request, 'blog/post_list.html', context_dict)
+    return render(request, 'blog/index.html', context_dict)
