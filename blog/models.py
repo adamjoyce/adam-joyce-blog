66688ff7c7a1e49537251948a1bfb38e6994b177
Adam Joyce <adamjoyce@outlook.com>
2017-11-08 14:42:09 +0000

    Featured header section now displaying most recent post with corresponding category background color

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.template.defaultfilters import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
+    categories = models.ManyToManyField(Category)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post
 def post_list(request):
     context_dict = {}
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
-            'published_date')
+            '-published_date')
+    featured_post = posts[0]
     context_dict['posts'] = posts
+    context_dict['featured_post'] = featured_post
     return render(request, 'blog/post_list.html', context_dict)
